e6497ba14e298c2b435b3134742acbc376e96f00
Maxim <Maximo02@gmx.de>
2025-11-03 13:58:02 +0100

    - broker test to look for mqtt status - test 2, removed "--quit" from def broker_active - updated logic for checking if broker is installed, if not install.

--- broker.py
+++ broker.py
@@ -1,12 +1,41 @@
 import subprocess
 
+def broker_installed():
+    result = subprocess.run(["mosquitto", "-h"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return result.returncode == 0
+
+def install_broker():
+    install_mosquitto = subprocess.run(["sudo", "apt", "install", "-y", "mosquitto"], capture_output=True, text=True)
+    if install_mosquitto.returncode != 0:
+        print("mosquitto installation failed:", install_mosquitto.stderr)
+        return False
+    install_client = subprocess.run(["sudo", "apt", "install", "-y" "mosquitto-clients"], capture_output=True, text=True)
+    if install_client.returncode != 0:
+        print("Client installation failed:", install_client.stderr)
+        return False
+    return True
+
 def broker_active():
     result = subprocess.run(
         ["systemctl", "is-active", "mosquitto"]
     )
     return result.returncode == 0
 
+if broker_installed():
+    print("broker is installed")
+else:
+    print("broker is not installed")
+    if input("Do you want to install it? (yes/no) ").strip().lower() == "yes":
+        if install_broker():
+            print("Broker and client installed successfully")
+        else:
+            print("installation failed")
+            exit(1)
+
+
 if broker_active():
     print("broker is running")
 else:
     print("broker is not running")
+
+
