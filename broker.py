c2c42582c2f2f9548ae1f96f79e50df06a017188
Ne1ght <Maximo02@gmx.de>
2025-11-05 20:47:10 +0100

    - changed the order of executions by making a tmux start.py to first create the window session and then run the python code to avoid it not being passed along

--- broker.py
+++ broker.py
@@ -36,6 +36,7 @@ def start_process(process_name):
     result = subprocess.run(["python3", process_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return result.returncode == 0
 
+subprocess.run(["tmux", "attach", "-t", "mqtt_session"])
 
 
 if broker_installed(): #calls the functions to check if the broker is installed and if not installs it
