4f9f30b804f2393e2a25c389afac996820e473f8
Ne1ght <Maximo02@gmx.de>
2025-11-05 20:59:03 +0100

    - changed the order of executions by making a tmux start.py to first create the window session and then run the python code to avoid it not being passed along

--- tmux_%_startup.py
+++ tmux_%_startup.py
@@ -13,7 +13,7 @@ def install_tmux(): #installs tmux
     return True
 
 def created_tmux_session(): #starts tmux to manage the mqtt session
-    result = subprocess.run(["tmux", "new-session", "-d", "-s", "mqtt_session", "python 3 broker.py"], capture_output=True, text=True)
+    result = subprocess.run(["tmux", "new-session", "-d", "-s", "mqtt_session", "python3 broker.py"], capture_output=True, text=True)
     if result.returncode != 0:
         print("tmux session creation failed:", result.stderr)
         return False
