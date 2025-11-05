5605127682cb6c416382d11ac5014e14cd33fb15
Ne1ght <Maximo02@gmx.de>
2025-11-05 21:19:13 +0100

    - added start.py to call tmux.py and broker.py to hopefully fooking work now im tierd send help ARGRGRGRRGR

--- start.py
+++ start.py
@@ -2,16 +2,13 @@ import subprocess
 import time
 
 subprocess.run(["python3", "tmux.py"])
-
+"""
 for i in range(10, 0, -1):
     print(f"Attaching tmux in {i} seconds")
     time.sleep(1)
+"""
 subprocess.run(["tmux", "attach", "-t", "mqtt_session"])
-for i in range(10, 0, -1):
-    print(f"waiting to attach: {i} seconds")
-    time.sleep(1)
 
-subprocess.run([
-    "tmux", "new-session", "-d", "-s", "mqtt_session",
-    ";", "send-keys", "python3 broker.py", "Enter"
-], shell=True)
+
+cmd = 'tmux new-session -d -s mqtt_session \; send-keys "python3 broker.py" Enter'
+subprocess.run(cmd, shell=True)
