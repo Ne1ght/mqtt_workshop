4eb8dd617cbe6c6f5042546291d00a09e1f845ce
Ne1ght <Maximo02@gmx.de>
2025-11-05 21:45:18 +0100

    - added start.py to call tmux.py and broker.py to hopefully fooking work now im tierd send help ARGRGRGRRGR

--- subscriber.py
+++ subscriber.py
@@ -1,5 +1,5 @@
 import subprocess
 
-subprocess.run(["tmux", "split-window", "-h", "-t", "mqtt_session:0.0"])
+subprocess.run(["tmux", "split-window", "-h", "-t", "mqtt_session:0.1"])
 subprocess.run(["tmux", "send-keys", "-t", "mqtt_session:0.2",
                 "mosquitto_sub -h localhost sensor1/motd", "C-m"])
