9da0f71066dd751e7125fecbbec2c354916135d2
Maxim <Maximo02@gmx.de>
2025-11-05 11:47:21 +0100

    - broker test to look for mqtt status - test 2, removed "--quit" from def broker_active - updated logic for checking if broker is installed, if not install. - added "," between -y and mosquitto-clients as it was forgotten - added info text for the current functions and if statements - added function to check if the publisher and subscriber are already running for now - using pgrep instead of systemctl is-active in is_running(pub oder sub) - added logic to check, install and start tmux to manage broker, publisher and subscriber in one ssh window - removed -d from the tmux starting to show it immediately - added wait for the tmux creation with input() so the operator can see the print output status of the checks - added the start logic for the publisher and subscriber to start with a dummy topic and open tmux splits on there own - added paramaters inside publisher and subscriber to be able to use subprocesses in an tmux session

--- publisher.py
+++ publisher.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.run(["tmux", "split-window", "-v", "-t", "mqtt_session"])
-subprocess.run(["mosquitto_pub", "-h", "localhost", "-t", "sensor1/motd", "-m", "Good morning!"])
+subprocess.run(["tmux", "send-keys", "-t", "mqtt_session:0.0" "mosquitto_pub", "-h", "localhost", "-t", "sensor1/motd", "-m", "Good morning!", "C-m"])
