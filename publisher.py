fd4f1676b8824d14f40b105da51db8a63cc63c15
Maxim <Maximo02@gmx.de>
2025-11-06 11:09:40 +0100

    - changed the execution order to avoid execution problems

--- publisher.py
+++ publisher.py
@@ -1,5 +1,5 @@
 import subprocess
 
 subprocess.run(["tmux", "split-window", "-v", "-t", "mqtt_session:0.0"])
-subprocess.run(["tmux", "send-keys", "-t", "mqtt_session:0.1",
-                "mosquitto_pub -h localhost -t sensor1/motd -m Good morning!", "C-m"])
+cmd = "tmux send-keys -t mqtt_workshop:0.1 \; mosquitto_pub -h localhost -t sensor1/motd -m Good space Mornin! C-m enter"
+subprocess.run(cmd, shell=True, capture_output=True)
