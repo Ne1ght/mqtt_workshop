32686e5eb80d50616e85a35d814656a01aa3932d
Maxim <Maximo02@gmx.de>
2025-11-06 12:00:34 +0100

    - changed the execution order to avoid execution problems

--- publisher.py
+++ publisher.py
@@ -1,5 +1,23 @@
 import subprocess
+import paho.mqtt.client as mqtt
+import time
+import random
+import json
 
 subprocess.run(["tmux", "split-window", "-v", "-t", "mqtt_session:0.0"])
 subprocess.run(["tmux", "send-keys", "-t", "mqtt_session:0.1",
                 'mosquitto_pub -h localhost -t sensor1/motd -m "Good morning!"', "C-m"])
+
+broker = "mqtt.eclipseprojects.io"
+topic = "demo/sensor/temp_feuchte"
+
+client = mqtt.Client()
+client.connect(broker, 1883, 60)
+
+while True:
+    temperatur = round(random.uniform(20.0, 30.0), 2)
+    feuchtigkeit = round(random.uniform(20.0, 70.0), 2)
+    daten = json.dumps({"temperatur": temperatur, "feuchtigkeit": feuchtigkeit})
+    client.publish(topic, daten)
+    print(f"Gesendet: {daten}")
+    time.sleep(20)
--- subscriber.py
+++ subscriber.py
@@ -1,5 +1,28 @@
 import subprocess
+import paho.mqtt.client as mqtt
+import json
 
 subprocess.run(["tmux", "split-window", "-h", "-t", "mqtt_session:0.1"])
 subprocess.run(["tmux", "send-keys", "-t", "mqtt_session:0.2",
                 "mosquitto_sub -h localhost -t sensor1/motd", "C-m"])
+
+broker = "mqtt.eclipseprojects.io"
+topic = "demo/sensor/temp_feuchte"
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Verbunden zum Broker")
+        client.subscribe(topic)
+    else:
+        print("Verbindungsfehler:" rc)
+
+def on_message(client, userdata, msg):
+    daten = json.loads(msg.payload.decode())
+    print(f"Empfangen - Temperatur: {daten['teemperatur']}°C, Feuchtigkeit: {daten['feuchtigkeit']}%")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker, 1883, 60)
+client.loop_forever()
